bbff1d26b94dfde4e32ec55b3bcb809f1f60ea2f
Mikerich94 <45273195+Mikerich94@users.noreply.github.com>
2026-04-04 11:07:45 -0400

    Update [slug].js

--- pages/recipes/[slug].js
+++ pages/recipes/[slug].js
@@ -1,105 +1,111 @@
-  import { createClient } from 'contentful'
-  import { documentToReactComponents } from '@contentful/rich-text-react-renderer';
-  import Image from 'next/image'
+import Head from 'next/head'
+import { createClient } from 'contentful'
+import { documentToReactComponents } from '@contentful/rich-text-react-renderer'
+import Image from 'next/image'
 import Skeleton from '../../components/Skeleton'
-  
-  const client = createClient({
-    space: process.env.CONTENTFUL_SPACE_ID,
-    accessToken: process.env.CONTENTFUL_ACCESS_KEY,
+
+const client = createClient({
+  space: process.env.CONTENTFUL_SPACE_ID,
+  accessToken: process.env.CONTENTFUL_ACCESS_KEY,
+})
+
+export const getStaticPaths = async () => {
+  const res = await client.getEntries({
+    content_type: 'recipe',
   })
 
-  export const getStaticPaths = async () => {
-const res = await client.getEntries({
-  content_type: 'recipe'
-})
+  const paths = res.items.map(item => ({
+    params: { slug: item.fields.slug },
+  }))
 
-const paths = res.items.map(item => {
   return {
-    params: { slug: item.fields.slug }
+    paths,
+    fallback: true,
   }
-})
-
-return {
-  paths,
-  fallback:true
 }
-  }
 
-export async function getStaticProps({params}) {
-const { items } = await client.getEntries({
-  content_type: 'recipe',
-  'fields.slug' : params.slug  
-})
+export async function getStaticProps({ params }) {
+  const { items } = await client.getEntries({
+    content_type: 'recipe',
+    'fields.slug': params.slug,
+  })
 
-if (!items.length) {
-  return {
-    redirect: {
-      destination: '/',
-      permanent: false
+  if (!items.length) {
+    return {
+      redirect: {
+        destination: '/',
+        permanent: false,
+      },
     }
   }
-}
 
-return {
-  props: { recipe: items[0] },
-  revalidate: 1
-}
+  return {
+    props: { recipe: items[0] },
+    revalidate: 1,
+  }
 }
 
 export default function RecipeDetails({ recipe }) {
-
   if (!recipe) return <Skeleton />
 
-  const { featuredImage, title, cookingTime, ingredients, method } = recipe.fields 
-  return (
-    <div>
-      <div className="banner">
-        <Image 
-        src={'https:' + featuredImage.fields.file.url}
-        width={featuredImage.fields.file.details.image.width}
-        height={featuredImage.fields.file.details.image.height}
-       style={{ objectFit: 'cover', width: '100%', height: 'auto', maxHeight: '375px' }}
-  alt="Thumbnail"
+  const { featuredImage, title, cookingTime, ingredients, method } = recipe.fields
 
-        />
-        <h2> { title }</h2>
-      </div>
-      <div className="info">
-        <p> Takes about { cookingTime } mintues to cook. </p>
-        <h3>Ingredients:</h3>
-        {ingredients.map(ing => (
-          <span key={ing}>{ing}</span>
-        ))}
-      </div>
-      <div className="method">
-        <h3>Method:</h3>
-        <div>{documentToReactComponents(method)}</div>
-      </div>
+  return (
+    <>
+      <Head>
+       <title>{`${title} | Mike's Recipes`}</title>
+        <meta name="description" content={`Recipe for ${title}`} />
+      </Head>
+
+      <div>
+        <div className="banner">
+          <Image
+            src={'https:' + featuredImage.fields.file.url}
+            width={featuredImage.fields.file.details.image.width}
+            height={featuredImage.fields.file.details.image.height}
+            style={{ objectFit: 'cover', width: '100%', height: 'auto', maxHeight: '375px' }}
+            alt={title}
+          />
+          <h2>{title}</h2>
+        </div>
+        <div className="info">
+          <p>Takes about {cookingTime} minutes to cook.</p>
+          <h3>Ingredients:</h3>
+          {ingredients.map(ing => (
+            <span key={ing}>{ing}</span>
+          ))}
+        </div>
+        <div className="method">
+          <h3>Method:</h3>
+          <div>{documentToReactComponents(method)}</div>
+        </div>
         <style jsx>{`
-        h2,h3 {
-          text-transform: uppercase;
-        }
-        .banner h2 {
-          margin: 0;
-          background: #fff;
-          display: inline-block;
-          padding: 20px;
-          position: relative;
-          top: -60px;
-          left: -10px;
-          transform: rotateZ(-1deg);
-          box-shadow: 1px 3px 5px rgba(0,0,0,0.1);
-        }
-        .info p {
-          margin: 0;
-        }
-        .info span::after {
-          content: ", ";
-        }
-        .info span:last-child::after {
-          content: ".";
-        }
-      `}</style>
-    </div>
+          h2,
+          h3 {
+            text-transform: uppercase;
+          }
+          .banner h2 {
+            margin: 0;
+            background: #fff;
+            display: inline-block;
+            padding: 20px;
+            position: relative;
+            top: -60px;
+            left: -10px;
+            transform: rotateZ(-1deg);
+            box-shadow: 1px 3px 5px rgba(0, 0, 0, 0.1);
+          }
+          .info p {
+            margin: 0;
+          }
+          .info span::after {
+            content: ', ';
+          }
+          .info span:last-child::after {
+            content: '.';
+          }
+        `}</style>
+      </div>
+    </>
   )
 }
